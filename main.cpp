4a3a1b5e237e571c5a0afae53a378f1b75ada08e
abc <2743630636@qq.com>
2018-06-23 01:55:35 +0800

    hhh

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include"number.h"
+int main()
+{
+	Number *p;
+	double q, w;
+	cin >> q>>w;
+	RealNumber(q);
+	RealNumber(w);
+	RealNumber E = RealNumber(q).add(RealNumber(w));
+	p = &E;
+	(*p).print();
+	IntegerNumber R = IntegerNumber(q).add(IntegerNumber(w));
+	p = &R;
+	(*p).print();
+	complex<double> q2, w2;
+	cin >> q2 >> w2;
+	ComplexNumber T = ComplexNumber(q2).add(ComplexNumber(w2));
+	p = &T;
+	(*p).print();
+	return 0;
+}
---
+++ number.h
@@ -0,0 +1,57 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+#include<iostream>
+#include<complex>
+using namespace std;
+class Number
+{
+public:
+	virtual void print()=0;
+};
+class RealNumber:public Number
+{
+public:
+	double x;
+	RealNumber() {}
+	RealNumber(double x1) { x = x1; }
+	void print()
+	{
+		cout << x << endl;
+	}
+	RealNumber add(const RealNumber& a)
+	{
+		return RealNumber(x + a.x);
+	}
+	RealNumber mul(const RealNumber& a)
+	{
+		return RealNumber(x*a.x);
+	}
+};
+class IntegerNumber:public RealNumber
+{
+public:
+	IntegerNumber() {};
+	IntegerNumber(int x2) { x = x2; }
+	IntegerNumber(const RealNumber& a) { x = int(a.x); }
+};
+class ComplexNumber :public Number
+{
+public:
+	complex<double> x;
+	ComplexNumber() {}
+	ComplexNumber(complex<double> x3) { x = x3; }
+	void print()
+	{
+		cout << x << endl;
+	}
+	ComplexNumber add(const ComplexNumber& a)
+	{
+		return ComplexNumber(x + a.x);
+	}
+	ComplexNumber mul(const ComplexNumber& a)
+	{
+		return ComplexNumber(x*a.x);
+	}
+};
+#endif
+#pragma once
